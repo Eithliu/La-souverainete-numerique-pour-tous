3cb27ef9b1ddcb4f88f15264304470a0905a7782
Eithliu <iris.benoit-martin@pix.fr>
2026-02-27 14:42:14 +0100

    feat(app): redo init projet

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script setup></script>
+
+<template>
+  <header>
+    <title>La souveraineté numérique chez soi</title>
+  </header>
+  <main>
+    <h1>La souveraineté numérique chez soi</h1>
+    <h2>La MeJ qui te fait reprendre la main sur tes données</h2>
+    <button>Mais d'abord, un petit Quiz !</button>
+  </main>
+</template>
+
+<style>
+* {
+  font-family: "Roboto", "Arial", sans-serif;
+  font-size: 16px;
+  color: rgb(86 86 92);
+}
+
+main {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+h1,
+h2 {
+  text-align: center;
+}
+
+h1 {
+  padding: 1rem;
+  font-size: 24px;
+}
+
+h2 {
+  padding: 0.8rem;
+  font-size: 20px;
+}
+
+button {
+  padding: 0.8rem 1.5rem;
+  border: none;
+  background: #5e6c84;
+  color: #fff;
+  border-radius: 5px;
+  width: fit-content;
+  margin: auto;
+}
+</style>
